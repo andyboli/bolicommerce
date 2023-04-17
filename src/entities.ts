97b288fa298fc00c376065d23dce256adc3ba60f
andyboli <webdevelopmentbolivar@gmail.com>
2023-04-17 08:52:54 -0300

    create entities

--- src/entities.ts
+++ src/entities.ts
@@ -1,16 +1,42 @@
+export enum DOMAIN_CATEGORIES {
+  "ALL" = "all",
+  "BUSCAPE" = "buscape",
+  "FREEEMARKET" = "freemarket",
+}
+
 export enum PRODUCTS_CATEGORIES {
-  "MOBILE" = "MOBILE",
-  "REFRIGERATOR" = "REFRIGERATOR",
-  "TV" = "TV",
+  "MOBILE" = "mobile",
+  "REFRIGERATOR" = "refrigerator",
+  "TV" = "tv",
 }
 
-export enum WEB_CATEGORIES {
-  "BUSCAPE" = "BUSCAPE",
-  "FREEE_MARKET" = "FREEE_MARKET",
-  "ALL" = "ALL",
+export interface Product {
+  category: PRODUCTS_CATEGORIES;
+  description: string;
+  domain: Omit<DOMAIN_CATEGORIES, DOMAIN_CATEGORIES.ALL>;
+  image: string;
+  price: string;
+  title: string;
+  url: string;
 }
 
-export interface Categories {
-  webCategory?: WEB_CATEGORIES;
-  productCategory?: PRODUCTS_CATEGORIES;
+export type DomainsProducs = Record<
+  DOMAIN_CATEGORIES.BUSCAPE | DOMAIN_CATEGORIES.FREEEMARKET,
+  Product[] | []
+>;
+
+export type Categories = Partial<
+  Pick<PostProductsRequestParams, "domain" | "category">
+>;
+
+export interface PostProductsRequestParams {
+  domain: DOMAIN_CATEGORIES;
+  category: PRODUCTS_CATEGORIES;
+  searchValue: string;
+  stored: boolean;
+}
+
+export interface PostProductsRequestResponse {
+  products: DomainsProducs;
+  stored: boolean;
 }
